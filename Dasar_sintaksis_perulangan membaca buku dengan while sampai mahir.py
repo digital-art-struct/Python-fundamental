d0ca57d0d27e3f2a4172bb6aeb2e09c631eeaf51
digital-art-struct <emkahauclick@gmail.com>
2024-01-26 14:57:47 +0700

    Rename in English

--- Dasar_sintaksis_perulangan membaca buku dengan while sampai mahir.py
+++ Dasar_sintaksis_perulangan membaca buku dengan while sampai mahir.py
@@ -1,28 +1,28 @@
 """
 Program perulangan membaca buku dengan While sampai mahir
 """
-jumlah_buku = 10
+book_count = 10
 print('Ibu berkata, "Baca semua bukumu"')
-total_jumlah_dibaca = 0
+read_count = 0
 
 
-jumlah_buku_yang_sudah_dibaca_dan_dipahami = 0
-print(f'Jumlah_buku_yang_sudah_dibaca_dan_dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
+understood_count = 0
+print(f'Jumlah_buku_yang_sudah_dibaca_dan_dipahami {understood_count}')
 
-while total_jumlah_dibaca < jumlah_buku * 2:
-    total_jumlah_dibaca = total_jumlah_dibaca + 1
-    if jumlah_buku_yang_sudah_dibaca_dan_dipahami == 9:
-        print(f"Buku ke{jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1} belum_paham")
+while read_count < book_count * 2:
+    read_count = read_count + 1
+    if understood_count == 9:
+        print(f"Buku ke{understood_count + 1} belum_paham")
     else:
-        jumlah_buku_yang_sudah_dibaca_dan_dipahami = jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1
-        print(f'Buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami} sudah dibaca_dan_dipahami')
+        understood_count = understood_count + 1
+        print(f'Buku ke {understood_count} sudah dibaca_dan_dipahami')
 
-print(f'Jumlah_buku_yang_sudah_dibaca_dan_dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
-if jumlah_buku_yang_sudah_dibaca_dan_dipahami == jumlah_buku:
+print(f'Jumlah_buku_yang_sudah_dibaca_dan_dipahami {understood_count}')
+if understood_count == book_count:
     print("Bu, semua buku sudah dibaca dan dipahami ")
 else:
     print(f"Bu, tidak semua buku bisa dipahami, "
-          f"Budi hanya bisa memahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami} buku")
+          f"Budi hanya bisa memahami {understood_count} buku")
 
 
 
